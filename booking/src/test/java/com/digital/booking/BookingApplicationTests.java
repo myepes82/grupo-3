abdc65620324b1bc922c32faccb735fb28768b06
Marlon Yepes <myepes82@misena.edu.co>
2022-10-18 21:21:26 -0500

    Initial commit

---
+++ booking/src/test/java/com/digital/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
